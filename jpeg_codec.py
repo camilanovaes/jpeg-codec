b2dc46242cd28a8c6c4a40c6c27f11e2cee6a68f
Pablo Wesley <wesley_pablo68@yahoo.com>
2019-12-19 00:50:30 -0300

    update jpeg codec steps

--- jpeg_codec.py
+++ jpeg_codec.py
@@ -1,5 +1,6 @@
 import argparse
 import jpeg.encoder, jpeg.decoder
+import numpy as np
 from PIL import Image
 
 parser = argparse.ArgumentParser(description="JPEG codec")
@@ -8,14 +9,27 @@ parser.add_argument('-f', '--filename',
 args = parser.parse_args()
 
 # Read image
-img = Image.open(args.filename)
+img     = Image.open(args.filename)
+src_img = np.asarray(img).astype(np.float64)
+
 img_width, img_height = img.size
 
 # Encoder
-encoder  = jpeg.encoder.Encoder(img)
-qnt_coef = encoder.process()
+encoder    = jpeg.encoder.Encoder(img)
+compressed = encoder.process()
+header     = compressed['header']
 
 # Decoder
-decoder  = jpeg.decoder.Decoder(qnt_coef, (encoder.width, encoder.height))
-decoder.process()
+decoder     = jpeg.decoder.Decoder(src_img, header, compressed,
+                                   (encoder.width, encoder.height))
+decoded_img = decoder.process()
+
+# Print image and calculate the error
+img       = np.asarray(decoded_img).copy()
+img_final = img[:img_height, :img_width, :]
+
+print(f'Error: {np.mean(src_img - img_final.astype(np.float64))}')
+
+img_final = Image.fromarray(img_final)
+img_final.show()
 
